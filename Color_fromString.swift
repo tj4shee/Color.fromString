9a652b01fe464514f3fd63886b216bd0d8b600e0
TJ <tj4shee@icloud.com>
2022-07-09 10:30:35 -0400

    Create Color_fromString.swift

---
+++ Color_fromString.swift
@@ -0,0 +1,53 @@
+extension Color {
+	static func fromString(_ colorStr: String) -> Color {
+		/***
+		 Meant to be used as a return to Color - works on all but the SwiftUI 'named' colors ie. .gray
+		 	from Color.description format of "#xxxxxxxx"
+		 				-or-
+		 	from a return of ColorPicker with the following starting values split by spaces
+		 		UIExtendedSRGBColorSpace (5 values)
+		 		kCGColorSpaceModelMonochrome (3 values)
+		 		kCGColorSpaceModelRGB (5 values)
+		 		kCGColorSpaceModelCMYK (6 values)
+		 		where values are separated by spaces
+		 */
+		var red = 0.8, green = 0.8, blue = 0.8, opacity = 1.0 //defaults to gray on errors
+		if (colorStr.count == 9) && (colorStr.first == "#") {
+			//Color.description from Color(red:green:blue:opacity)
+			red = Double(UInt32(String(Array(colorStr)[1...2]), radix: 16) ?? 0) / 255.0
+			green = Double(UInt32(String(Array(colorStr)[3...4]), radix: 16) ?? 0) / 255.0
+			blue = Double(UInt32(String(Array(colorStr)[5...6]), radix: 16) ?? 0) / 255.0
+			opacity = Double(UInt32(String(Array(colorStr)[7...8]), radix: 16) ?? 0) / 255.0
+		} else {
+			let values = colorStr.split(separator: " ")
+			if values.count == 6 {
+				//CMYK - Not an exact match, but ok for choosing control colors
+				let cyan = (1.0 - (Double(values[1]) ?? 0.0))
+				let magenta = (1.0 - (Double(values[2]) ?? 0.0))
+				let yellow = (1.0 - (Double(values[3]) ?? 0.0))
+				let black = (1.0 - (Double(values[4]) ?? 0.0))
+				red = (cyan * black)
+				green = (magenta * black)
+				blue = (yellow * black)
+				opacity = (Double(values[5]) ?? 0.0)
+			} else {
+				if values.count == 3 {
+					//Monochrome
+					red = (Double(values[1]) ?? 0.0)
+					green = red
+					blue = red
+					opacity = (Double(values[2]) ?? 0.0)
+				} else {
+					if values.count == 5 {
+						//RGB
+						red = (Double(values[1]) ?? 0.0)
+						green = (Double(values[2]) ?? 0.0)
+						blue = (Double(values[3]) ?? 0.0)
+						opacity = (Double(values[4]) ?? 0.0)
+					}
+				}
+			}
+		}
+		return Color(red: red, green: green, blue: blue, opacity: opacity)
+	}
+}
